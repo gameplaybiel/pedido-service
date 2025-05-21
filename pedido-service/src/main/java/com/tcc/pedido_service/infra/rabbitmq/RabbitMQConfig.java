c03c38f0de59b803c2fe7e55c4b4b3f5c32b27a0
Gabriel <gabrielconceicao569@gmail.com>
2025-05-21 12:50:14 -0300

    Updates of project

--- pedido-service/src/main/java/com/tcc/pedido_service/infra/rabbitmq/RabbitMQConfig.java
+++ pedido-service/src/main/java/com/tcc/pedido_service/infra/rabbitmq/RabbitMQConfig.java
@@ -1,47 +1,25 @@
 package com.tcc.pedido_service.infra.rabbitmq;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-    // Nomes consistentes com os usados no Publisher
-    public static final String PEDIDO_EXCHANGE = "pedido.exchange";
-    public static final String PEDIDO_CRIADO_QUEUE = "pedido.criado.queue";
-    public static final String PEDIDO_DELETADO_QUEUE = "pedido.deletado.queue";
-    public static final String CRIADO_ROUTING_KEY = "pedido.criado";
-    public static final String PEDIDO_DELETADO_ROUTING_KEY = "pedido.deletado";
+    @Value("${pedido.queue.name}")
+    private String pedidoQueueName;
+
+    @Value("${pagamento.queue.name}")
+    private String pagamentoQueueName;
 
     @Bean
-    public DirectExchange pedidoExchange() {
-        return new DirectExchange(PEDIDO_EXCHANGE);
+    Queue pedidoQueue() {
+        return new Queue(pedidoQueueName, true);
     }
 
     @Bean
-    public Queue pedidoCriadoQueue() {
-        return new Queue(PEDIDO_CRIADO_QUEUE, true);
-    }
-
-    @Bean
-    public Queue pedidoDeletadoQueue() {
-        return new Queue(PEDIDO_DELETADO_QUEUE, true);
-    }
-
-    @Bean
-    public Binding bindingCriado() {
-        return BindingBuilder.bind(pedidoCriadoQueue())
-                .to(pedidoExchange())
-                .with(CRIADO_ROUTING_KEY);
-    }
-
-    @Bean
-    public Binding bindingDeletado() {
-        return BindingBuilder.bind(pedidoDeletadoQueue())
-                .to(pedidoExchange())
-                .with(PEDIDO_DELETADO_ROUTING_KEY);
+    Queue pagamentoQueue() {
+        return new Queue(pagamentoQueueName, true);
     }
 }
